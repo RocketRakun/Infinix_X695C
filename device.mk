d16e1fe7c916b248da54c3c5ef0fddc6edb4496f
RocketRakun <138932464+RocketRakun@users.noreply.github.com>
2025-06-28 00:24:14 +0700

    Infinix-X695C: move twrp_.mk makefile

--- device.mk
+++ device.mk
@@ -16,12 +16,6 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota.mk)
 
 # Enable project quotas and casefolding for emulated storage without sdcardfs
 $(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
-
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.crypto.dm_default_key.options_format.version=2 \
-    ro.crypto.volume.filenames_mode=aes-256-cts \
-    ro.crypto.volume.metadata.method=dm-default-key \
-    ro.crypto.volume.options=::v2
     
 ENABLE_VIRTUAL_AB := true
 AB_OTA_UPDATER := true
