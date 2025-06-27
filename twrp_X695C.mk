d9e09ac3c7c7bba9319df6360d1c9c02883132ee
RocketRakun <138932464+RocketRakun@users.noreply.github.com>
2025-06-28 00:36:45 +0700

    Infinix-X695C: d16e1fe & add build ID

--- twrp_X695C.mk
+++ twrp_X695C.mk
@@ -10,11 +10,17 @@ $(call inherit-product, device/infinix/X695C/device.mk)
 # Inherit some common TWRP stuff.
 $(call inherit-product, vendor/twrp/config/common.mk)
 
+# Enable proper FBE v2 decryption for Android 11+ (especially for GSI)
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.crypto.dm_default_key.options_format.version=2 \
     ro.crypto.volume.filenames_mode=aes-256-cts \
     ro.crypto.volume.metadata.method=dm-default-key \
     ro.crypto.volume.options=::v2
+
+# Override fingerprint & build description (for AVB, OTA, and compatibility)
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.build.fingerprint=Infinix/X695C-GL/Infinix-X695C:11/RP1A.200720.011/221114V356:user/release-keys \
+    ro.build.description="vnd_x695_h854-user 11 RP1A.200720.011 156472 release-keys"
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := X695C
