020a88d1323738b4174e5e998bff0a47d4033d6b
RocketRakun <138932464+RocketRakun@users.noreply.github.com>
2025-06-28 00:25:09 +0700

    Infinix-X695C: d16e1fe

--- twrp_X695C.mk
+++ twrp_X695C.mk
@@ -10,6 +10,12 @@ $(call inherit-product, device/infinix/X695C/device.mk)
 # Inherit some common TWRP stuff.
 $(call inherit-product, vendor/twrp/config/common.mk)
 
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.crypto.dm_default_key.options_format.version=2 \
+    ro.crypto.volume.filenames_mode=aes-256-cts \
+    ro.crypto.volume.metadata.method=dm-default-key \
+    ro.crypto.volume.options=::v2
+
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := X695C
 PRODUCT_NAME := twrp_X695C
